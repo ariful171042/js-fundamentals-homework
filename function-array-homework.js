2dfc5c9086553120b3f6d4d8136a275107daa1ff
ariful171042 <arif171042@gmail.com>
2023-04-16 06:11:19 +0600

    particle

---
+++ function-array-homework.js
@@ -0,0 +1,7 @@
+// Functions and Arrays HW:
+
+/* 01. You are given an array of car objects, each with a "name" and "topSpeed" property. You also have a river object with a "name" and "currentSpeed" property. Write a function that takes in the car array and river object as arguments and returns the name of the car with the highest net speed (topSpeed - currentSpeed) across the river. */
+
+/* 02. You have an array of shopping items, each with a "name" and "price" property. You also have a budget limit. Write a function that takes in the shopping array and budget as arguments and returns the total price of the items that fit within the budget. */
+
+/* 03. You are given an array of numbers. Write a function that takes in this array and another function as inputs. The input function should perform a transformation (double each element etc.) on each element in the array. The main function should then return the transformed array. */
